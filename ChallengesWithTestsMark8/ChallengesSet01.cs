5114b0c38a91c87900b263c76c4f4a7aecb0d41f
Troy Maxwell <zephyrmaxwell@hotmail.com>
2019-09-11 09:11:50 -0500

    completed Challenge Set 1

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -13,32 +13,50 @@ namespace ChallengesWithTestsMark8
         }
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            double difference = minuend - subtrahend;
+            return difference;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int sum = number1 + number2;
+            return sum;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            long multi = factor1 * factor2;
+            return multi;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return "Hello, " + nameOfPerson + "!";
+            }
+
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
